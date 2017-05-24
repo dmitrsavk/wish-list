49aeba1846666b72c328f384efd137de25e154fb
dmitrsavk <dmitrsavk@yandex.ru>
2017-05-24 22:42:00 +0300

    Add skeleton of project

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './App.scss';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='App'>
+                <h1>Login page</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
